748ff9e4998bab4c30d7196e373bf1f07cf3e238
dpsales <8014899+dpsales@users.noreply.github.com>
2024-11-17 23:38:01 -0300

    dando erro no periodo

--- autosei.py
+++ autosei.py
@@ -21,6 +21,7 @@ from selenium.common.exceptions import NoSuchElementException, StaleElementRefer
 
 
 MODULE_DIR = Path(__file__).parent
+PASSWORD_FILE = Path.home().joinpath(".password.txt")
 
 
 logging.basicConfig(level=logging.INFO)
@@ -32,11 +33,6 @@ def build_path(subfolder):
     if not os.path.exists(folderpath): os.makedirs(folderpath)
     return folderpath
 
-
-
-
-
-
 def busca_documentos(
     # TO DO: entrar com o endereço SEI 
     url, 
@@ -46,7 +42,7 @@ def busca_documentos(
     period = None,  # (start_date, end_date)
     output_dir="extraidos",
     charset="iso-8859-1",
-    passwordfile='.password/password.txt'
+    passwordfile=PASSWORD_FILE
 ):
     # nomes_arquivos = build_path('nomes_arquivos')
     _output_dir = build_path(output_dir)
@@ -277,7 +273,8 @@ def carregar_janela_principal():
     from tkinter import ttk   
     from pathlib import Path
     from tkcalendar import DateEntry
-f   rom ttkthemes import ThemedTk
+    from ttkthemes import ThemedTk
+    from threading import Thread
     
     class CustomThread(Thread):    
         def __init__(self, group=None, target=None, name=None,
@@ -295,103 +292,180 @@ f   rom ttkthemes import ThemedTk
     
     FONT = "Arial"
 
-    janela = tk.Tk()
-    janela.title("Tirando dados do SEI")
-    janela.geometry("900x600")
-    janela.config(bg="lightblue")
-    janela.resizable(False, False)
-    janela.iconbitmap(MODULE_DIR.joinpath("resources", "logo-sei.ico"))
-    janela.rowconfigure(0, weight=1)
-    janela.columnconfigure([5,5], weight=2)
-
-    img = tk.PhotoImage(
-        file=MODULE_DIR.joinpath("resources", "logo-sei.png"), 
-        width=300, 
-        height=200
-    )
+    def abrir_segunda_janela():
+        janela2=tk.Toplevel()
+        janela2.title("Janela de Login")
+        janela2.config(bg="lightblue")
+        janela2.resizable(False, False)
+        janela2.iconbitmap(MODULE_DIR.joinpath("logo-sei.ico"))
+        janela2.geometry("300x200")
+        
+        
+        label_login = tk.Label(janela2, text = "Login", bg="lightblue", font = (FONT, 12, "bold"))
+        label_login.grid(row = 0, column = 0, padx=1, pady=3)
+        entry_login = ttk.Entry(janela2, width=15)
+        entry_login.grid(row=0, column= 1, padx=2, pady=3)
+        
+        label_password = tk.Label(janela2, text = "Password", bg="lightblue", font = (FONT, 12, "bold"))
+        label_password.grid(row = 1, column = 0, padx=1, pady=4)
+        entry_password = ttk.Entry(janela2, width= 15, show = "*")
+        entry_password.grid(row=1, column= 1, padx=2, pady=4)
+        
+        def save_arq():
+            with open(PASSWORD_FILE, "w") as arq:
+                arq.write(f'{entry_login.get()}:{entry_password.get()}')
+                #fechar a janela de login
+            janela2.destroy()
+                               
+        botao_voltar = ttk.Button(janela2, text = 'Salvar o Login e a senha ', style="big.TButton", command = save_arq)
+        botao_voltar.grid(row = 2, column = 0, columnspan=2)
     
+    janela = ThemedTk(theme="adapta")
+    janela.title("Tirando dados do SEI")  
+    
+    # janela.geometry("900x600")
+    janela.config(bg="lightblue")
+    janela.iconbitmap(MODULE_DIR.joinpath("logo-sei.ico"))
+
+    img = tk.PhotoImage(file = MODULE_DIR.joinpath("logo-sei.png"), width=300, height=200)
     logo = tk.Label(janela, image=img, background="lightblue")
 
     logo.grid(row=0, column= 0, padx=1, pady=3)
-
-    mensagem1 = tk.Label(
-        janela,
-        text="Busca de dados dos documentos SEI",
-        fg="gray",
-        bg ="lightblue",
-        font=(FONT, "20"),
-        width=80,
-        height=6
+    
+    mensagem1 = tk.Label(janela,
+                        text="Busca de dados dos documentos SEI",
+                        fg="gray",
+                        #  image= img,
+                        bg ="lightblue",
+                        font=(FONT,"20"),
+                        width=30,
+                        height=5
     )
 
-    mensagem1.grid(
-        row=0, 
-        column= 1, 
-        columnspan=20, 
-        padx=1, 
-        pady=3
+    mensagem1.grid(row=0, column= 1, columnspan=20, padx=1, pady=3)
+
+    #criar o Entry
+    label_url = ttk.Label(janela,
+                        text="URL do sistema SEI *",
+                        background="lightblue",
+                        font=(FONT)
     )
 
-    # criar o Entry
-    label_url = ttk.Label(
-        janela,
-        text="Url do sistema SEI",
-        font=FONT
+    label_url.grid(row=2,
+                column= 0,
+                padx=10,
+                pady=5,
+                sticky="nsew"
     )
 
-    label_url.grid(
-        row=2,
-        column= 0,
-        padx=10,
-        pady=5,
-        sticky="nsew"
+    entry_url = ttk.Entry(janela,
+                        width=15
     )
 
-    entry_url = ttk.Entry(
-        janela,
-        width=35
+    entry_url.grid(row=2,
+                column=1,
+                columnspan=8,
+                padx=10,
+                pady=5,
+                sticky="nsew"
     )
 
-    entry_url.grid(
-        row=2,
-        column=1,
-        columnspan=8,
-        padx=10,
-        pady=5,
-        sticky="nsew"
+    label_doc_type = ttk.Label(janela,
+                            text="Qual é o tipo de documento que quer os dados?*",
+                            background="lightblue",
+                            font=(FONT)
+    )
+    label_doc_type.grid(row=3,
+                        column=0,
+                        padx=10,
+                        pady=5,
+                        sticky="nsew"
     )
 
-    label_doc_type = ttk.Label(
-        janela,
-        text="Qual é o tipo de documento que quer os dados?", 
-        font=FONT
+    entry_doc_type = ttk.Entry(janela,
+                            width=20
+    )
+
+    entry_doc_type.grid(row=3,
+                        column=1,
+                        # columnspan=8,
+                        padx=10,
+                        pady=5,
+                        sticky="nsew"
     )
     
-    label_doc_type.grid(
-        row=3,
-        column=0,
-        padx=10,
-        pady=5,
-        sticky="nsew"
+    label_obrigacao = ttk.Label(janela,
+                        text="* São obrigatórios",
+                        background="lightblue",
+                        font= (FONT, 14, "bold"),
+                        foreground="red"
+    )
+    
+    label_obrigacao.grid(row=4,
+                        # columnspan=2,
+                        padx=10,
+                        pady=5
+    )
+    label_datainicial = ttk.Label(janela,
+                        text="Data de inicio da pesquisa:", 
+                        background="lightblue",
+                        font=(FONT)
+    )
+    label_datainicial.grid(row=5,
+                        column=0,
+                        padx=10,
+                        pady=5,
+                        sticky="nsew")
+
+            
+    entry_datainicial = DateEntry(janela,
+                                dateformat = "%d/%m/%Y", 
+                                selectmode = 'day'                                          
+                                )
+
+    entry_datainicial.grid(row=5,
+                        column=1,
+                        # padx=8,
+                        # pady=5
+    )
+       
+
+    label_datafinal = ttk.Label(janela,
+                        text="Data de inicio da pesquisa:",
+                        background="lightblue", 
+                        font=(FONT)
+    )
+    label_datafinal.grid(row=6,
+                        column=0,
+                        # padx=8,
+                        # pady=5,
+                        sticky="nsew"
     )
 
-    entry_doc_type = ttk.Entry(
-        janela, 
-        width=35
+    entry_datafinal = DateEntry(janela,
+                                dateformat = "%d/%m/%Y", 
+                                selectmode = 'day'
+    )
+    # def select_datef():
+    #     datef = entry_datafinal.get_date()
+    #     date_label.config(text = datef)
+    
+    entry_datafinal.grid(row=6,
+                        column=1,
     )
 
-    entry_doc_type.grid(
-        row=3,
-        column=1,
-        columnspan=8,
-        padx=10,
-        pady=5,
-        sticky="nsew"
+  
+    botao_login = ttk.Button(janela,
+                            text = "Login",
+                           #  font=(FONT, "12", "bold"),
+                            style="big.TButton",
+                           command=abrir_segunda_janela
     )
-
-    # Estilo do botão
-    s = ttk.Style()
-    s.configure("big.TButton", fg='black', bg="darkgray", font = (FONT, 16))
+    botao_login.grid(row=7,
+                    columnspan=2,
+                    padx=15,
+                    pady=5
+    )
     
     # manipulador de evento click
     def schedule_check(t):
@@ -421,7 +495,9 @@ f   rom ttkthemes import ThemedTk
         # Start the crawler in a new thread.
         url = entry_url.get()
         doc_type = entry_doc_type.get()
-        t = CustomThread(target=busca_documentos, args=(url, doc_type))
+        data_inicial = entry_datainicial.get_date()
+        data_final = entry_datafinal.get_date()
+        t = CustomThread(target=busca_documentos, args=(url, doc_type, data_inicial, data_final))
         t.start()
         # Start checking periodically if the thread has finished.
         schedule_check(t)
@@ -435,8 +511,8 @@ f   rom ttkthemes import ThemedTk
     )
     
     botao_entrada.grid(
-        row=4,
-        columnspan=7,
+        row=10,
+        columnspan=2,
         padx=10,
         pady=5
     )
@@ -472,7 +548,7 @@ def main():
     parser.add_argument("--url", required=run_in_commandline, help="URL do SEI a ser pesquisado")
     parser.add_argument("--doc", required=run_in_commandline, help="Tipo do documento SEI")
     parser.add_argument("--salvar", required=run_in_commandline, help="Diretório para salvar os resultados", type=Path)
-    parser.add_argument("--passwordfile", required=run_in_commandline, help="Arquivo com a senha do SEI, em ASCII", default='.password/password.txt')
+    parser.add_argument("--passwordfile", required=run_in_commandline, help="Arquivo com a senha do SEI, em ASCII", default=PASSWORD_FILE)
     
     # argumentos opcionais para linha de comando
     parser.add_argument("-di", "--data-inicio", type=date, help="Data de início da pesquisa", dest="di")
